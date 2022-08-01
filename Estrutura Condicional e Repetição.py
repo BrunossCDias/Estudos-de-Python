cbf11a1c752780689310866bc5748a6a88c115e4
Bruno Dias <108904391+BrunossCDias@users.noreply.github.com>
2022-08-01 09:05:41 -0300

    Update Estrutura Condicional e Repetição.py

--- Estrutura Condicional e Repetição.py
+++ Estrutura Condicional e Repetição.py
@@ -1,6 +1,6 @@
-#utilizar estruturas condicionais simples e compostas nos seus programas em Python.
-#aplicando os comandos if: e else: no Python.
+#utilizando estruturas condicionais aninhadas simples, e compostas, aplicando os comandos if:, elif, e else:
 
+print ('Exemplo: ')
 
 
 nome = str(input('Qual seu nome? '))
@@ -19,28 +19,28 @@ print('Tenho um Bom Dia, {}'.format(nome))
 #Exercicios
 
 print('Exercicio 1')
-'''Crie um programa que eprove um emprestimo bancario para a compra de uma casa
+'''Programa que avalie um emprestimo bancario para a compra de uma casa
 O programa vai perguntar o valor da casa, o salario do comprador e em quantos 
 anos ele vai pagar a divida.
 Calcule o valor da prestação mensal, sabendo-se que ele nao pode exceder 30% do salario
-ou então o emprestimo sera negado'''
+ou então o emprestimo sera negado.'''
 
 casa = float(input('Qual o valor a ser financiado ? '))
 salario = float(input('Qual salario mensal ?'))
-anos = int (input('Quantos anos vai pagar ?'))
+anos = int (input('Em quantos anos vai pagar ?'))
 prestacao =  casa / (anos * 12)
 minimo = salario * 30 / 100
 print("Para pagar uma casa de R${:.2f} em {} anos".format(casa,anos))
 print('a prestacão será de R$ {:.2f}'.format(prestacao))
 if prestacao <= minimo:
-    print('Você pode realizar o financiamento')
+    print('Você pode realizar o financiamento.')
 else:
-    print('Você não pode realizar o financiamento')
+    print('Você não pode realizar o financiamento.')
 
 
 
 print('Exercicio 2')
-'''# Ecreva um programa que leia um numero inteiro qualquer e peça para o usuario escolher qual sera a 
+'''Programa que leia um numero inteiro qualquer e peça para o usuario escolher qual sera a 
 base de conversão.'''
 numero = int(input('Qual numero inteiro ?' ))
 print('''Escolha uma das opções abaixo para conversão :
@@ -49,43 +49,43 @@ print('''Escolha uma das opções abaixo para conversão :
 [3] converter para EXADECIMAL''')
 opcao = int(input('Sua opção: '))
 if opcao == 1:
-    print('{} Convertido para BINARIO é igual a {}'.format(numero, bin(numero)[2::]))
+    print('{} Convertido para BINARIO é igual a {}.'.format(numero, bin(numero)[2::]))
 elif opcao == 2:
-    print('{} Convertido para OCTAL é {}'.format(numero, oct(numero)[2::]))
+    print('{} Convertido para OCTAL é {}.'.format(numero, oct(numero)[2::]))
 elif opcao == 3:
-    print('{} Convertido para EXADECIMAL é {}'.format(numero, hex(numero)[2::]))
+    print('{} Convertido para EXADECIMAL é {}.'.format(numero, hex(numero)[2::]))
 
 
 print('Exercicio 3')
-'''Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
-- O primeiro valor é maior
-- O segundo valor é maior
-- Não existe valor maior, os dois são iguais'''
+'''Programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
+- O primeiro valor é maior.
+- O segundo valor é maior.
+- Não existe valor maior, os dois são iguais.'''
 
 numero1 = int (input('Digite um numero :'))
 numero2 = int (input('Digite outro numero :'))
 if numero1 > numero2:
-    print(' O numero {} é maior que o numero {}'.format(numero1, numero2))
+    print(' O numero {} é maior que o numero {}.'.format(numero1, numero2))
 elif numero2 > numero1:
-    print('O meior {} é maior que o numero {}'.format(numero2, numero1))
+    print('O meior {} é maior que o numero {}.'.format(numero2, numero1))
 else:
-    print('Não existe valor maior, os dois são iguais')
+    print('Não existe valor maior, os dois são iguais.')
 
 print('Exercicio 4')
-'''Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade,
+'''Programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade,
 se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do 
-tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.'''
+tempo do alistamento. Programa também deverá mostrar o tempo que falta ou que passou do prazo.'''
 
 from datetime import date
 ano = date.today().year
 nascimento = int(input('Qual ano de nascimento ? '))
 idade = ano - nascimento
-print('Quem nasceu em {} tem {} anos em {}'.format(nascimento, idade, ano))
+print('Quem nasceu em {} tem {} anos em {}.'.format(nascimento, idade, ano))
 if idade == 18:
-    print('Você ja deve se alistar este ano !')
+    print('Você ja deve se alistar este ano. !')
 elif idade < 18:
     faltam = 18 - idade
-    print('Você tem {} anos e  ainda faltam {} anos para o seu alistamento'.format(idade,faltam))
+    print('Você tem {} anos e  ainda faltam {} anos para o seu alistamento.'.format(idade,faltam))
     anoa = ano + faltam
     print('O ano do seu alistamento será em {} !'.format(anoa))
 elif idade > 18:
@@ -93,45 +93,9 @@ elif idade > 18:
     print('Você ja tem {} anos e deveria ter se alistado a {} anos. '.format(idade, faltam))
     anob = ano - faltam
     print('Você deveria ter se alistado em {} !'.format(anob))
-
-
+    
 print('Exercicio 5')
 #Crie um programa que jogue com o usuario, PREDRA,PAPEL e TESOURA.
-
-from time import sleep
-chance = str(input('''Escolha uma opcão abaixo: 
-[1] PEDRA
-[2] PAPEL
-[3] TESOURA'''))
-print('JO')
-sleep(1)
-print('KEN')
-sleep(1)
-print('PÔ !!!')
-
-
-
-# Estruta versatil com o laço FOR
-
-s = 0
-for vezes in range(0, 4):
-    valor = int(input('Digite um valaor: '))
-    s += valor + 1
-print('A soma de todos os valores é {}'.format(s))
-print('\033[0;31;40mFIM ! \033[m')
-
-
-
-inicio = int(input('Inicio : '))
-fim = int(input('Fim : '))
-passo = int(input('Passo :'))
-for repetir in range (inicio, fim+1, passo ):
-    print(repetir)
-
-print('\033[0;31;40mFIM ! \033[m')
-
-
-print('Exercicio 6')
 
 from time import sleep
 from random import randint
@@ -180,3 +144,34 @@ if computador == 2:
         print('O Computador \033[0;31.40mVENCEU !\033[m')
     elif jogador == 0:
         print('O Computador \033[0;;31;40mPERDEU !\033[m')
+
+
+
+print('Exercicio 6')
+
+# Iteração com laço de repetição em uma estruta versatil com o FOR.
+
+
+s = 0
+for vezes in range(0, 4):
+    valor = int(input('Digite um valaor: '))
+    s += valor + 1
+print('A soma de todos os valores é {}'.format(s))
+print('\033[0;31;40mFIM ! \033[m')
+
+
+
+inicio = int(input('Inicio : '))
+fim = int(input('Fim : '))
+passo = int(input('Passo :'))
+for repetir in range (inicio, fim+1, passo ):
+    print(repetir)
+
+print('\033[0;31;40mFIM ! \033[m')
+
+#Exercicios
+
+
+
+
+
