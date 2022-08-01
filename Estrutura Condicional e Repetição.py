92df3eb753600c9dbddd25e9dd9b81ddeb2e2342
Bruno Dias <108904391+BrunossCDias@users.noreply.github.com>
2022-08-01 14:44:04 -0300

    Update Estrutura Condicional e Repetição.py

--- Estrutura Condicional e Repetição.py
+++ Estrutura Condicional e Repetição.py
@@ -199,4 +199,18 @@ for n in range(1, 501, 2):
 print(f"A soma de todos os {c} valores solicitados é {s}.")
 
 
+print('Exercicio 3')
+#programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
+#Se o valor digitado for ímpar, desconsidere-o.
+
+contador1 = 0
+somador = 0
+for repetir in range (1, 7):
+    numero2 = int(input('Digite o {}˚ valor: '.format(repetir)))
+    if numero % 2 == 0:
+        somador += numero2
+        contador1 += 1
+print('Voce informou {} numeros PAERES, e a soma de todos eles é {}'.format(contador1, somador))
+
+
 
