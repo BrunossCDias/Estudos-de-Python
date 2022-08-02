2cefa1539a0da4e0eeb93e0cc2f13f510ce7e376
Bruno Dias <108904391+BrunossCDias@users.noreply.github.com>
2022-08-02 10:40:45 -0300

    Update Exercícios.py

--- Estrutura condicional aninhadas/Exercícios.py
+++ Estrutura condicional aninhadas/Exercícios.py
@@ -1,3 +1,19 @@
+
+
+# Exemplo .
+
+nome = str(input('Qual seu nome? '))
+if nome == 'Bruno':
+    print( 'Que nome bonito !')
+elif nome == 'Gustavo' or nome == 'Amanda' or nome == 'Bruna':
+    print('Seu nome é bem popular no Brasil !')
+elif nome in 'Paula Joao Batista Carmen':
+    print('Que belo nome !'.format(nome))
+else:
+    print('Seu nome é bem normal !')
+print('Tenho um Bom Dia, {}'.format(nome))
+
+
 #Exercicios
 
 print('Exercicio 1')
