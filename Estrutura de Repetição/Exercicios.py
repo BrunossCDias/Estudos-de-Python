b404747bda51b7022cf4ddb08206fdf830846b29
Bruno <brunosscd@outlook.com>
2022-08-04 14:12:08 -0300

    + Revisao Exer OA

--- Estrutura de Repetição/Exercicios.py
+++ Estrutura de Repetição/Exercicios.py
@@ -128,3 +128,21 @@ while opcao != 5:
     print('=-='*10)
     print(sleep(2))
 print('FIM')
+
+
+print ('Exercicio 6')
+
+from random import randint
+# Programa em que o computador vai "pensar" em um número entre 0 e 10.
+# O jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites
+# foram necessários para vencer.
+
+computador = randint(0, 10)
+print('Computador pensou em um numero entre 0 e 10, tente adivinhar..')
+acertou = False
+jogadas = 0
+while not acertou:
+    jogador = int(input('Qual seu palpite ? '))
+    jogadas += 1
+    if jogador == computador:
+        acertou == True
--- Estrutura de Repetição/rascunho.py
+++ Estrutura de Repetição/rascunho.py
@@ -1,42 +1,16 @@
-from time import sleep
+print ('Exercicio 6')
 
-print ('Exercicio 5')
-'''Programa que leia dois valores e mostre um menu na tela:
-[ 1 ] somar
-[ 2 ] multiplicar
-[ 3 ] maior
-[ 4 ] novos números
-[ 5 ] sair do programa
-O programa deverá realizar a operação solicitada em cada caso.'''
+from random import randint
+# Programa em que o computador vai "pensar" em um número entre 0 e 10.
+# O jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites
+# foram necessários para vencer.
 
-
-
-numer1 = int(input('Digite um numero: '))
-numer2 = int(input("Digite outro numero: "))
-opcao = 0
-while opcao != 5:
-    print ('''[ 1 ] somar
-[ 2 ] Multiplicar
-[ 3 ] Maior
-[ 4 ] Novos números
-[ 5 ] Sair do programa ''')
-    opcao = int(input("======> Escolha uma opção: "))
-    if opcao == 1:
-        print(f'O resutado da soma entre os dois valore é {numer1+numer2}.')
-    elif opcao == 2:
-        print(f'O resutado da {numer1} x {numer2} é {numer1*numer2}.')
-    elif opcao == 3:
-        if numer1 > numer2:
-            maior = numer1
-        else:
-            maior = numer2
-        print(f'Entre o {numer1} e o {numer2}  numero maior é {maior}.')
-    elif opcao == 4:
-        print('Informe os valores novamente !')
-        numer1 = int(input('Digite um valor: '))
-        numer2 = int(input('Digite outro valor: '))
-    else:
-        print('Opção invalida. Tente novamente.')
-    print('=-='*10)
-    print(sleep(2))
-print('FIM')
+computador = randint(0, 10)
+print('Computador pensou em um numero entre 0 e 10, tente adivinhar..')
+acertou = False
+jogadas = 0
+while not acertou:
+    jogador = int(input('Qual seu palpite ? '))
+    jogadas += 1
+    if jogador == computador:
+        acertou == True
