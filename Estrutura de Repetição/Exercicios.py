0411964dcdca5af99a5d339b4c1b97de1fe4a66a
Bruno Dias <108904391+BrunossCDias@users.noreply.github.com>
2022-08-04 22:53:23 -0300

    Update Exercicios.py

--- Estrutura de Repetição/Exercicios.py
+++ Estrutura de Repetição/Exercicios.py
@@ -157,9 +157,8 @@ print ('Exercicio 7')
 #Programa que lê um primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos
 # da progressão usando a estrutura while.
 
-primeiro = int(input('Insira um termo: '))
+termo = int(input('Insira um termo: '))
 razao = int(input('Insira uma razão da PA: '))
-termo = primeiro
 cont = 1
 while cont <= 10:
     print(f'{termo}')
