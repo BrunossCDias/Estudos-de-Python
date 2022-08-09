5071c741e27ee55388b808e47bae3060c6c5e056
Bruno <brunosscd@outlook.com>
2022-08-09 10:40:44 -0300

    Novo Exerci

--- Estrutura de Repetição/Exercicio 11.py
+++ Estrutura de Repetição/Exercicio 11.py
@@ -1,4 +1,3 @@
-
 #   Programa que leia o nome, idade e sexo de 4 pessoas. No final do programa,
 # mostre: a média de idade do grupo, qual é o nome do homem mais velho e quantas
 # mulheres têm menos de 20 anos.
@@ -29,9 +28,3 @@ mediaidade = somaidade / 4
 print(f'A média de idade do grupo é de {mediaidade}')
 print(f'O homem mais velho tem {maioridadehomem} anos, e se chama {nomevelho}')
 print(f'O total sao {totmulh20} de mulhres com menos de 20 anos  ')
-
-
-
-
-
-
