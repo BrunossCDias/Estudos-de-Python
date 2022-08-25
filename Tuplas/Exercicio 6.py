b0e41704cf6775a1fb919e701fa4b2d536b4632e
Bruno <brunosscd@outlook.com>
2022-08-25 08:51:52 -0300

    Desafio Ex 6 Atualizando

--- Tuplas/Exercicio 6.py
+++ Tuplas/Exercicio 6.py
@@ -1,9 +1,9 @@
 
 # Programa que tenha uma tupla com várias palavras (não usar acentos).
 # Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
-tupla_de_palavras = ('aaeeiioouu','amendoin', 'carro', 'pastel',
-                     'dipirona', 'programador', 'python', 'notebook',
-                     'frutas',)
+tupla_de_palavras = ('amendoin', 'carro', 'pastel',
+                     'dipirona', 'programador', 'python',
+                     'notebook', 'frutas',)
 for palavra in tupla_de_palavras:
     print(f'\nNa palavra {palavra.upper()}, tem as vogais = ', end='')
     vogais_encontradas = []
@@ -14,11 +14,12 @@ for palavra in tupla_de_palavras:
     print(sorted(vogais_encontradas),end= ' ')
 
 
-''' Defini uma tupla chamada tupla_de_palavras com diversos indices '''
+''' Defini uma tupla chamada tupla_de_palavras '''
 ''' Criei um loop FOR palavra para cada item IN tupla_de_palavras. '''
 ''' Imprimi  cada indice da tupla formatado. '''
-''' A variavel vogais_encontradas recebe uma lista'''
-''' Criei loop FOR para cada letra na vaiavel palavra  '''
-''' Defini um IF identado no loop se a letras minuscula estiver IN "aeiou" '''
-''' Dentro da identação do IF criei mais um IF se a letra nao estiver na variavel vogal_encontradas, a variavel recebe com um metodo .append a letra em maiusculo. '''
+''' Variavel vogais_encontradas recebe uma lista. '''
+''' Criei loop FOR para cada letra na variavel palavra  '''
+''' Defini um IF identado no loop se a letras estiver IN "aeiou" '''
+''' Dentro da identação do IF criei mais um IF se a letra nao estiver na variavel vogal_encontradas,
+ a variavel recebe com um metodo .append a letra em maiusculo. '''
 ''' Na ultima linha fora do loop eu imprimi a variavel com todas as vogais sem repetição'''
