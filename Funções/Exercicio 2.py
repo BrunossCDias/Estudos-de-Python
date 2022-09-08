bc44425df8234afb3a33f3438b44152b2cc42dc9
Bruno <brunosscd@outlook.com>
2022-09-08 15:17:35 -0300

    Melhorado a descrição do programa.

--- Funções/Exercicio 2.py
+++ Funções/Exercicio 2.py
@@ -15,6 +15,6 @@ escreva('Cursando Pyhton Junior')
 escreva('Hotmart')
 
 ''' Criei uma def chamada escreva com parametro chamado txt.
-    Criei outra variael identada a função chamada tam, que vai receber o len do parametro txt somando mais 4 indices. 
+    Criei outra variavel identada a função chamada tam, que vai receber o len do parametro txt somando mais 4 indices. 
     Idetado a def, criei um print para uma impressão do simbolo tio, vezes a variavel tam
     criei mais um print formatado o paraentro txt com dois espaços no inicio para centralizar '''
