48802f8a65496706a0f4716fa1b0a073851a9f62
Bruno <brunosscd@outlook.com>
2022-09-09 16:20:59 -0300

    Programa que analisa valores, e mostra o maior.

---
+++ Funções/Exercicio 4.py
@@ -0,0 +1,23 @@
+# Programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros.
+# Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+from time import sleep
+
+
+def maior(*núm):
+    cont = maior = 0
+    print('\nAnalisando os valores inseridos')
+    for n in núm:
+        print(f'{n}', end=' ')
+        sleep(0.5)
+        if cont == 0:
+            maior = n
+        else:
+            if n > maior:
+                maior = n
+        cont += 1
+    print(f'Foi inserido {cont} valores ao todo. ')
+    print(f'O maior valor informado foi {maior}. ')
+
+
+maior(2, 4, 8, 20)
+maior(3, 5, 9, 33)
