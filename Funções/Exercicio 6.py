f9d7bf812cb99e77bf091f8963a731a9c89cf780
Bruno <brunosscd@outlook.com>
2022-09-14 15:06:43 -0300

    corrigido o retonro do elif.

--- Funções/Exercicio 6.py
+++ Funções/Exercicio 6.py
@@ -2,7 +2,7 @@
 # uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
 
 
-# Importando classe date apenas dentro da função.
+# Importando a biblioteca datetime apenas dentro da função.
 def voto(ano):
     from datetime import date
     atual = date.today().year
@@ -10,7 +10,7 @@ def voto(ano):
     if idade < 16:
         return f'Com {idade} anos: NÃO VOTA'
     elif 16 <= idade < 18 or idade > 65:
-        return f'Com {idade} anos: O NÃO VOTA'
+        return f'Com {idade} anos: O VOTO É OPCIONAL'
     else:
         return f'Com {idade} anos: O VOTO É OBRIGATORIO'
 
