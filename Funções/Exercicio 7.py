dde0f56b5bfdcfcad82ac195112e062426698ea3
Bruno <brunosscd@outlook.com>
2022-09-14 15:05:25 -0300

    Programa que calcula o fatorial de um número.

---
+++ Funções/Exercicio 7.py
@@ -0,0 +1,26 @@
+
+# Programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e
+# outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de
+# cálculo do fatorial.
+
+
+def fatorial(n, show=False):
+    """
+    -> Calcula o fatorial de um número.
+    parametro n: número a ser calculado.
+    parametro show: opcional mostrar ou não a conta.
+    return: O valor do fatorial de um número n.
+    """
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= c
+    return f
+
+
+print(fatorial(10, show=True))
