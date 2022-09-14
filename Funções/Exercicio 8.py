1979dd5097965db3e6e737528906bd539c9a8586
Bruno <brunosscd@outlook.com>
2022-09-14 16:48:37 -0300

    Programa que recebe dados, e passando uma função dependendo de uma condição.

---
+++ Funções/Exercicio 8.py
@@ -0,0 +1,17 @@
+# Programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais: o nome de um jogador e quantos gols ele marcou.
+# O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado não tenha sido informado corretamente.
+
+def ficha(jogador='desconhecido', gols=0):
+    print(f'O jogador {jogador}, fez {gols} gols no campeonato.')
+
+
+n = str(input('Nome do jogador: '))
+g = str(input('Quantidade de gols: '))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == '':
+    ficha(gols=g)
+else:
+    ficha(n, g)
