bb77dd13f8c6293dda735161bcd12c927d799621
Bruno <brunosscd@outlook.com>
2022-09-19 16:56:14 -0300

    Correção de nome da função no programa.

--- Modulos, e Pacotes/Exercicio 1/Exercicio 1.py
+++ Modulos, e Pacotes/Exercicio 1/Exercicio 1.py
@@ -1,9 +1,9 @@
 # Programa que importe módulo e use algumas funções conforme a chamada do programa.
 
-from moeda import metade, dobro, aumentar, dimibuir
+from moeda import metade, dobro, aumentar, diminuir
 
 p = float(input('Digite o valor: '))
 print(f'A metade de {p}$ é {metade(p)}$')
 print(f'O dobro de {p}$, é {dobro(p)}$')
 print(f'Aumentando 10 % deste valor, temos um total de {aumentar(p, 10)}$')
-print(f'Diminuindo 20 % deste valor, temos um total de {dimibuir(p, 20)}$')
+print(f'Diminuindo 20 % deste valor, temos um total de {diminuir(p, 20)}$')
