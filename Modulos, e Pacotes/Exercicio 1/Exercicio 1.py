19e6847d88e298d85c31fe1fd0e4687933387009
Bruno <brunosscd@outlook.com>
2022-09-20 08:59:31 -0300

    Alterado a importação do modulo, e corrigido a chamada da função.

--- Modulos, e Pacotes/Exercicio 1/Exercicio 1.py
+++ Modulos, e Pacotes/Exercicio 1/Exercicio 1.py
@@ -1,9 +1,9 @@
 # Programa que importa módulo e use algumas funções conforme a chamada do programa.
 
-from uteis import moeda
+from moeda import metade, dobro, aumentar, diminuir
 
 p = float(input('Digite o valor: '))
-print(f'A metade de {p}$ é {moeda.metade(p)}$')
-print(f'O dobro de {p}$, é {moeda.dobro(p)}$')
-print(f'Mais 10 % deste valor, temos um total de {moeda.aumentar(p, 10)}$')
-print(f'Menos 20 % deste valor, temos um total de {moeda.diminuir(p, 20)}$')
+print(f'A metade de {p}$ é {metade(p)}$')
+print(f'O dobro de {p}$, é {dobro(p)}$')
+print(f'Mais 10 % deste valor, temos um total de {aumentar(p, 10)}$')
+print(f'Menos 20 % deste valor, temos um total de {diminuir(p, 20)}$')
