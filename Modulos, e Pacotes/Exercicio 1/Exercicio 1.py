16a2606117c0ce3ca706305dbef6c5d21b1182e7
Bruno <brunosscd@outlook.com>
2022-09-20 08:18:05 -0300

    Alterando mensagem de saida.

--- Modulos, e Pacotes/Exercicio 1/Exercicio 1.py
+++ Modulos, e Pacotes/Exercicio 1/Exercicio 1.py
@@ -1,9 +1,9 @@
-# Programa que importe módulo e use algumas funções conforme a chamada do programa.
+# Programa que importa módulo e use algumas funções conforme a chamada do programa.
 
 from moeda import metade, dobro, aumentar, diminuir
 
 p = float(input('Digite o valor: '))
 print(f'A metade de {p}$ é {metade(p)}$')
 print(f'O dobro de {p}$, é {dobro(p)}$')
-print(f'Aumentando 10 % deste valor, temos um total de {aumentar(p, 10)}$')
-print(f'Diminuindo 20 % deste valor, temos um total de {diminuir(p, 20)}$')
+print(f'Mais 10 % deste valor, temos um total de {aumentar(p, 10)}$')
+print(f'Menos 20 % deste valor, temos um total de {diminuir(p, 20)}$')
