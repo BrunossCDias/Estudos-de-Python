4b5f7121e5f64e1b0f9f2c85c4fbe8cbfdb5e722
Bruno <brunosscd@outlook.com>
2022-09-20 09:06:02 -0300

    Correção do nome do módulo, e a chamada do módulo comfunção com

--- Modulos, e Pacotes/Exercicio 2/Exercicio 2.py
+++ Modulos, e Pacotes/Exercicio 2/Exercicio 2.py
@@ -1,8 +1,8 @@
-import moeda
+import Moeda
 
 
 p = float(input('Digite o valor: '))
-print(f'A metade de {moeda.moeda(p)}, é {moeda.moeda(moeda.metade(p))}')
-print(f'O dobro de {moeda.moeda(p)}, é {moeda.moeda(moeda.dobro(p))}')
-print(f'Aumentando 10 % deste valor, temos um total de {moeda.moeda(moeda.aumentar(p, 10))}')
-print(f'Diminuindo 20 % deste valor, temos um total de {moeda.moeda(moeda.diminuir(p, 20))}')
+print(f'A metade de {Moeda.moeda(p)}, é {Moeda.moeda(Moeda.metade(p))}')
+print(f'O dobro de {Moeda.moeda(p)}, é {Moeda.moeda(Moeda.dobro(p))}')
+print(f'Aumentando 10 % deste valor, temos um total de {Moeda.moeda(Moeda.aumentar(p, 10))}')
+print(f'Diminuindo 20 % deste valor, temos um total de {Moeda.moeda(Moeda.diminuir(p, 20))}')
