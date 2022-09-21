d0ba415cc0b9a4af4baf5d9224ab1fe12d1d923a
Bruno <brunosscd@outlook.com>
2022-09-21 09:54:56 -0300

    Programa que uma nova função que resume o programa, tornando menor e mais compreensivel.

--- Modulos, e Pacotes/Exercicio 4/Exercicio 4.py
+++ Modulos, e Pacotes/Exercicio 4/Exercicio 4.py
@@ -1,6 +1,8 @@
-#
+# Com o mesmo programa de cálculos, adicione ao módulo uma função chamada resumo(), que mostre na tela algumas
+# informações geradas pelas funções que já temos no módulo criado até aqui.
 
 import Calculos
 
 
-p = float(input('Digite o valor: '))
+p = float(input('Digite o valor R$: '))
+Calculos.resumo(p)
