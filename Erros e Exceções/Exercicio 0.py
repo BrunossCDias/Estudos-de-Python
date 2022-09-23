f3ddcd1806dc55a63dd82490db47fc4aa2ab3626
Bruno <brunosscd@outlook.com>
2022-09-22 21:09:11 -0300

    Programa modularizado simples recebe valores e chama duas funções que faz o tratamento  dentro de um loop while, para o programa não parar com os Erros de valores inseridos.

---
+++ Erros e Exceções/Exercicio 0.py
@@ -0,0 +1,8 @@
+
+
+from interface import *
+
+
+n1 = (leiaint('Digite um valor int: '))
+n2 = (leiafloat('Digite um valor float '))
+print(f'O valor inteiro digitado foi {n1}, e valor real foi {n2} ')
