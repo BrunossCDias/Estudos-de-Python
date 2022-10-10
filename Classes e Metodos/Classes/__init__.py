50957428346f360b0d5fa8c36b1ea60ee17e448e
bruno.dias <brunosscd@outlook.com>
2022-10-10 13:47:38 -0300

    Classe utilizada para criar os objetos da introdução.

--- Classes e Metodos/Classes/__init__.py
+++ Classes e Metodos/Classes/__init__.py
@@ -1,48 +1,73 @@
 class Pessoa:
+    sexo : str ='F/M'
     def __init__(self, nome, idade, dirigindo=False, falando=False):
         self.nome = nome
         self.idade = idade
         self.dirigindo = dirigindo
         self.falando = falando
 
-    def dirigir(self):
+    def dirigir(self, veiculo):
         if self.dirigindo:
             print(f'{self.nome}, já esta dirigindo.')
-            return
-        if self.falando:
-            print(f'{self.nome} não pode dirigir falando no celular')
-            return
-
-
-        print(f'{self.nome} está dirigindo.')
-        self.dirigindo = True
-
-    def parardirigir(self):
-        if not self.dirigindo:
-            print(f'{self.nome} não esta dirigindo.')
-            return
         else:
-            print(f'{self.nome} parou de dirigir.')
+            print(f'{self.nome} está dirigindo o {veiculo}.')
+            self.dirigindo = True
+
+    def parardirigir(self, veiculo):
+        if not self.dirigindo:
+            print(f'{self.nome} não esta dirigindo {veiculo}.')
+
+        else:
+            print(f'{self.nome} parou de dirigir o {veiculo}.')
             self.dirigindo = False
 
-    def falar(self, assunto):
+    def falar(self, celular):
+
         if self.dirigindo:
-            print(f'{self.nome} não pode falar no {assunto} dirigindo !')
+            print(f'{self.nome} não pode falar no {celular} dirigindo !')
             return
         if self.falando:
-            print(f'{self.nome} ja está falando ! ')
+            print(f'{self.nome} ja está falando no {celular} ! ')
             return
 
-        print(f'{self.nome}, esta falando.')
+        print(f'{self.nome}, esta falando no {celular}.')
         self.falando = True
 
-
-    def pararfalar(self):
+    def pararfalar(self, celular):
         if not self.falando:
-            print(f'{self.nome}, não está falando.')
+            print(f'{self.nome}, não está falando no {celular}.')
             return
+        print(f'{self.nome}, parou de falar no {celular} !!')
+        self.falando = False
 
-        print(f'{self.nome}, parou de falar !!')
+    def estado(self):
 
-    def status(self):
-        print(f'Dirigindo: {self.dirigindo}, Falando: {self.falando} ')
+        return {'dirigindo': self.dirigindo, 'falando': self.falando}
+
+    def verifica_estado(self, dirigindo , falando):
+        validacao = (self.dirigindo == dirigindo and self.falando == falando)
+        print(f'validacao: {validacao}')
+        if (self.dirigindo == dirigindo and self.falando == falando ):
+            return True
+        return False
+
+
+
+
+    # 1 Crie uma nova função que chama verifica_estado.
+    # 2 Essa função deverá verificar se os parametros do estado esperado é igual o estado atual do objeto.
+    # 3 Deverá retornar true quando for igual e false quando algum estado for diferente.
+
+
+    ''' 
+    def verifica_estado(self, dirigindo, falando) -> bool:
+        if self.dirigindo == dirigindo and self.falando == falando:
+            return True
+        return False
+    '''
+
+
+    def imprime_estado(self):
+      return print(f'Dirigindo: {self.dirigindo}, Falando: {self.falando} ')
+
+
