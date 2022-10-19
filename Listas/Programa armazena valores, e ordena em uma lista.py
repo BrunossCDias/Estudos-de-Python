7273c4e6966c05e7f95400b4648fbd52fbea79a4
bruno.dias <brunosscd@outlook.com>
2022-10-19 10:24:21 -0300

    Inserido metodo .sort,e renomeado programa.

--- Listas/Programa armazena valores, e ordena em uma lista.py
+++ Listas/Programa armazena valores, e ordena em uma lista.py
@@ -1,6 +1,7 @@
 
 # Programa onde o usuário possa digitar vários valores numéricos e cadastre-os em uma lista. Caso o número já exista lá
 # dentro, ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
+
 numeros = list()
 while True:
     valores = input('Digite um valor: ')
@@ -12,4 +13,5 @@ while True:
     resposta = str(input('Deseja continuar: [S/N]'))
     if resposta in 'Nn':
         break
-print(f'Os numeros digitados foram:{sorted(numeros)}')
+numeros.sort()
+print(f'Os numeros digitados foram:{numeros}')
