c5a045ec808a7fa2631ff64bc39bb1603473112f
bruno.dias <brunosscd@outlook.com>
2022-10-19 10:28:13 -0300

    Rascunho de como criar uma lista, e adicionar elementos com o metodo .append, e utilizando a funcao enumerate em um loop.

--- Listas/Rascunho.py
+++ Listas/Rascunho.py
@@ -1,1 +1,13 @@
 
+valores = list()
+valores.append(5)
+valores.append(6)
+valores.append(4)
+
+for cont in range(0,3):
+    valores.append(int(input('Digite um valor: ')))
+
+for c, v in enumerate(valores):
+    print(f'Na posição {c+1}, encontrie o valor {v}')
+print('Cheguei ao final da lista')
+print(sorted(valores))
